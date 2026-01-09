f8558da664f34f95884e8432b49a6c0c6aa60e3d
230660221098 <230660221098@student.unsap.ac.id>
2026-01-09 11:12:03 +0700

    membuat sub direktori 230660221098

---
+++ 230660221098/praktikum10-16/routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+const verifyToken = require('../middlewares/auth');
+
+const registerController = require('../controllers/registerController');
+
+const loginController = require('../controllers/LoginConroller');
+
+const userController = require('../controllers/UserController');
+
+const { validateRegister, validateLogin } = require('../utils/validators/auth');
+
+const { validateUser } = require('../utils/validators/user');
+
+router.post('/register', validateRegister, registerController.register);
+
+router.post('/login', validateLogin, loginController.login);
+
+router.get('/admin/users', verifyToken, userController.findUsers);
+
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
+
+router.get('/admin/users/:id', verifyToken, userController.findUserById);
+
+router.put('/admin/users/:id', verifyToken, validateUser, userController.updateUser);
+
+router.delete('/admin/users/:id', verifyToken, userController.deleteUser);
+
+module.exports = router;
